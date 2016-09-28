9d47c5482bd9d6d23ab56a6556e50b69472497f7
Carlos Salinas Gancedo <carlos.salinas.gancedo@mendix.com>
2016-09-28 17:01:49 +0200

    922383# Fix Bug
    - In the message request build, the dialog counters passing in string value instead of decimal.
    - No delete current DialogNodes before to add new ones.

--- javasource/watsonservices/utils/ConversationService.java
+++ javasource/watsonservices/utils/ConversationService.java
@@ -1,7 +1,6 @@
 package watsonservices.utils;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,11 +20,11 @@ import com.mendix.systemwideinterfaces.core.IContext;
 import com.mendix.systemwideinterfaces.core.IMendixIdentifier;
 import com.mendix.systemwideinterfaces.core.IMendixObject;
 
+import watsonservices.proxies.Conversation;
 import watsonservices.proxies.ConversationContext;
 import watsonservices.proxies.ConversationEntity;
 import watsonservices.proxies.ConversationIntent;
 import watsonservices.proxies.ConversationMessageResponse;
-import watsonservices.proxies.Conversation;
 import watsonservices.proxies.DialogNode;
 
 public class ConversationService {
@@ -33,9 +32,8 @@ public class ConversationService {
 	private static final String WATSON_CONVERSATION_LOGNODE = "WatsonServices.IBM_WatsonConnector_Conversation";
 	private static final ILogNode LOGGER = Core.getLogger((Core.getConfiguration().getConstantValue(WATSON_CONVERSATION_LOGNODE).toString()));
 	private static final  com.ibm.watson.developer_cloud.conversation.v1_experimental.ConversationService service = new com.ibm.watson.developer_cloud.conversation.v1_experimental.ConversationService("2016-07-11");
-	private static final DecimalFormat conversationCountersFormat = new DecimalFormat("#0.0");
-	
-	static public IMendixObject sendMessage(IContext context, ConversationContext conversationContext, String input, String username, String password) throws CoreException, MendixException {
+
+	public static IMendixObject sendMessage(IContext context, ConversationContext conversationContext, String input, String username, String password) throws CoreException, MendixException {
 		LOGGER.debug("Executing Watson Send Message Connector...");
 
 		service.setUsernameAndPassword(username, password);
@@ -68,7 +66,7 @@ public class ConversationService {
 		return createMessageResponse(context, conversationContext, response);
 	}
 
-	static private MessageRequest createMessageRequest(ConversationContext conversationContext, String input) throws CoreException {
+	private static MessageRequest createMessageRequest(ConversationContext conversationContext, String input) throws CoreException {
 		MessageRequest messageRequest = null;
 		if(StringUtils.isNotEmpty(conversationContext.getConversationId())){
 			final Map<String, Object> conversationContextInput = new HashMap<String, Object>();
@@ -78,14 +76,12 @@ public class ConversationService {
 				dialogNodes.add(node.getName());
 			}
 
-			
 			final Map<String, Object> contextSystemInput = new HashMap<String, Object>();
-			contextSystemInput.put("dialog_stack", dialogNodes);
-			contextSystemInput.put("dialog_turn_counter", conversationCountersFormat.format(conversationContext.getDialogTurnCounter()));
-			contextSystemInput.put("dialog_request_counter", conversationCountersFormat.format(conversationContext.getDialogRequestCounter()));
-
+			contextSystemInput.put("dialog_stack", dialogNodes);		
 			conversationContextInput.put("system", contextSystemInput);
 			conversationContextInput.put("conversation_id", conversationContext.getConversationId());
+			conversationContextInput.put("dialog_turn_counter", conversationContext.getDialogTurnCounter());
+			conversationContextInput.put("dialog_request_counter", conversationContext.getDialogRequestCounter());
 
 			messageRequest = new MessageRequest.Builder()
 					  .inputText(input)
@@ -100,16 +96,48 @@ public class ConversationService {
 		return messageRequest;
 	}
 
+	private static IMendixObject createMessageResponse(IContext context, ConversationContext conversationContext, MessageResponse response) throws CoreException {
+		updateConversationContext(context, conversationContext, response);
+
+		final IMendixObject messageResponseObject = Core.instantiate(context, ConversationMessageResponse.entityName);
+		messageResponseObject.setValue(context, ConversationMessageResponse.MemberNames.ConversationId.toString(), response.getContext().get("conversation_id"));
+		messageResponseObject.setValue(context, ConversationMessageResponse.MemberNames.Input.toString(), response.getInputText());
+		messageResponseObject.setValue(context, ConversationMessageResponse.MemberNames.Output.toString(), response.getTextConcatenated(","));
+
+		Core.commit(context, messageResponseObject);
+
+		
+
+		for(Intent intent : response.getIntents()){
+			final IMendixObject intentObject = Core.instantiate(context, ConversationIntent.entityName);
+			intentObject.setValue(context, ConversationIntent.MemberNames.Name.toString(), intent.getIntent());
+			intentObject.setValue(context, ConversationIntent.MemberNames.Confidence.toString(), intent.getConfidence().toString());
+			intentObject.setValue(context, ConversationIntent.MemberNames.ConversationIntent_ConversationResponse.toString(), messageResponseObject.getId());
+
+			Core.commit(context, intentObject);
+		}
+		
+		for(Entity entity : response.getEntities()){
+			final IMendixObject entityObject = Core.instantiate(context, ConversationEntity.entityName);
+			entityObject.setValue(context, ConversationEntity.MemberNames.Name.toString(), entity.getEntity());
+			entityObject.setValue(context, ConversationEntity.MemberNames.Value.toString(), entity.getValue());
+			entityObject.setValue(context, ConversationEntity.MemberNames.ConversationEntity_ConversationResponse.toString(), messageResponseObject.getId());
+
+			Core.commit(context, entityObject);
+		}
+
+		return messageResponseObject;
+	}
+
 	@SuppressWarnings("unchecked")
-	static private IMendixObject createMessageResponse(IContext context, ConversationContext conversationContext, MessageResponse response) throws CoreException {
-		Map<String, Object> resposeSystemContext = updateConversationContext(context, conversationContext, response);
+	private static Map<String, Object> updateConversationContext(IContext context, ConversationContext conversationContext, MessageResponse response) throws CoreException {
+		deleteCurrentDialogStack(conversationContext);
 
-		final IMendixObject conversationMessageObject = Core.instantiate(context, ConversationMessageResponse.entityName);
-		conversationMessageObject.setValue(context, ConversationMessageResponse.MemberNames.ConversationId.toString(), response.getContext().get("conversation_id"));
-		conversationMessageObject.setValue(context, ConversationMessageResponse.MemberNames.Input.toString(), response.getInputText());
-		conversationMessageObject.setValue(context, ConversationMessageResponse.MemberNames.Output.toString(), response.getTextConcatenated(","));
-
-		Core.commit(context, conversationMessageObject);
+		conversationContext.setConversationId(context, response.getContext().get("conversation_id").toString());
+		Map<String, Object> responseContext = response.getContext();
+		Map<String, Object> resposeSystemContext = (Map<String, Object>) responseContext.get("system");
+		conversationContext.setDialogTurnCounter(new BigDecimal(resposeSystemContext.get("dialog_turn_counter").toString()));
+		conversationContext.setDialogRequestCounter(new BigDecimal(resposeSystemContext.get("dialog_request_counter").toString()));
 
 		List<String> dialogStack = (List<String>) resposeSystemContext.get("dialog_stack");
 		for(String dialogNode : dialogStack){
@@ -122,36 +150,13 @@ public class ConversationService {
 			Core.commit(context, dialogNodeObject);
 		}
 
-		for(Intent intent : response.getIntents()){
-			final IMendixObject conversationIntentObject = Core.instantiate(context, ConversationIntent.entityName);
-			conversationIntentObject.setValue(context, ConversationIntent.MemberNames.Name.toString(), intent.getIntent());
-			conversationIntentObject.setValue(context, ConversationIntent.MemberNames.Confidence.toString(), intent.getConfidence().toString());
-			conversationIntentObject.setValue(context, ConversationIntent.MemberNames.ConversationIntent_ConversationResponse.toString(), conversationMessageObject.getId());
-
-			Core.commit(context, conversationIntentObject);
-		}
-		
-		for(Entity entity : response.getEntities()){
-			final IMendixObject conversationEntityObject = Core.instantiate(context, ConversationEntity.entityName);
-			conversationEntityObject.setValue(context, ConversationEntity.MemberNames.Name.toString(), entity.getEntity());
-			conversationEntityObject.setValue(context, ConversationEntity.MemberNames.Value.toString(), entity.getValue());
-			conversationEntityObject.setValue(context, ConversationEntity.MemberNames.ConversationEntity_ConversationResponse.toString(), conversationMessageObject.getId());
-
-			Core.commit(context, conversationEntityObject);
-		}
-
-		return conversationMessageObject;
-	}
-
-	@SuppressWarnings("unchecked")
-	static private Map<String, Object> updateConversationContext(IContext context, ConversationContext conversationContext, MessageResponse response) throws CoreException {
-		conversationContext.setConversationId(context, response.getContext().get("conversation_id").toString());
-		Map<String, Object> responseContext = response.getContext();
-		Map<String, Object> resposeSystemContext = (Map<String, Object>) responseContext.get("system");
-		conversationContext.setDialogTurnCounter(new BigDecimal(resposeSystemContext.get("dialog_turn_counter").toString()));
-		conversationContext.setDialogRequestCounter(new BigDecimal(resposeSystemContext.get("dialog_request_counter").toString()));
-
 		conversationContext.commit();
 		return resposeSystemContext;
+	}
+
+	private static void deleteCurrentDialogStack(ConversationContext conversationContext) throws CoreException {
+		for(DialogNode dialogNode : conversationContext.getDialog_Stack()){
+			dialogNode.delete();
+		}
 	}
 }
