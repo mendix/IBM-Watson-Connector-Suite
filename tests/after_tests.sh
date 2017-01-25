273a1d5c681f5938cd7079644ce0d93afb0532d8
Raphaël Audet <raphael.audet@gmail.com>
2017-01-25 17:49:12 +0100

    1053641 fix typo

--- tests/after_tests.sh
+++ tests/after_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exu
 
-cf login -a $API_URL -u $USERNAME -p $PASSWORD -o $ORG -s $SPACE
+cf login -a $API_URL -u $USERNAME -p $PASSWORD
 cf delete-service-key my-travis-conv-service-blab keyasstring -f
 cf ds my-travis-conv-service-blab -f
 
--- tests/before_tests.sh
+++ tests/before_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exu
 
-cf login -a $API_URL -u $USERNAME -p $PASSWORD -o $ORG -s $SPACE
+cf login -a $API_URL -u $USERNAME -p $PASSWORD
 cf cs conversation free my-travis-conv-service-blab
 cf create-service-key my-travis-conv-service-blab keyasstring
 cf service-key my-travis-conv-service-blab keyasstring
